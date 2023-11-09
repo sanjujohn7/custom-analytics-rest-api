8ffcaa7252e85fea2da7e0ac9aa1af85ef4d98b2
sanju <sanju.john@edstem.com>
2023-11-09 13:05:05 +0530

    exception handler changes

--- src/main/java/com/example/saasplatform1/customanalyticsrestapi/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/saasplatform1/customanalyticsrestapi/exception/GlobalExceptionHandler.java
@@ -1,8 +1,38 @@
 package com.example.saasplatform1.customanalyticsrestapi.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    public String handleMissingServletRequestParameterException(MissingServletRequestParameterException ex) {
+        return ex.getMessage();
+    }
+    @ExceptionHandler(FileNotPresentException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    public String handleFileNotPresentException(FileNotPresentException ex){
+    return ex.getMessage();
+}
+    @ExceptionHandler(CustomDataUploadException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    public String handleCustomDataUploadException(CustomDataUploadException ex){
+        return ex.getMessage();
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    public String handleUserNotFoundException(UserNotFoundException ex){
+        return ex.getMessage();
+    }
 
 }
