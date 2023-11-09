0da9f915ee9a80f538986b60b502ed1507f3200a
aswathy suresh <uthram113@gmail.com>
2023-11-09 21:21:12 +0530

    added controller test cases

--- src/test/java/com/example/saasplatform1/customanalyticsrestapi/controller/CustomAnalyticsControllerTest.java
+++ src/test/java/com/example/saasplatform1/customanalyticsrestapi/controller/CustomAnalyticsControllerTest.java
@@ -1,8 +1,11 @@
 package com.example.saasplatform1.customanalyticsrestapi.controller;
 
 
+import com.example.saasplatform1.customanalyticsrestapi.contract.CustomAnalyticsDataFilterRequest;
 import com.example.saasplatform1.customanalyticsrestapi.contract.CustomAnalyticsDataResponse;
+import com.example.saasplatform1.customanalyticsrestapi.contract.GetTotalProfitAndCountResponse;
 import com.example.saasplatform1.customanalyticsrestapi.service.CustomAnalyticsService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -11,9 +14,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
 
 import static org.hamcrest.Matchers.is;
@@ -25,7 +31,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@AutoConfigureTestDatabase
+@WithMockUser
 public class CustomAnalyticsControllerTest {
 
     @Autowired
@@ -89,4 +95,112 @@ public class CustomAnalyticsControllerTest {
                 .andExpect(header().string("Content-Type", "text/csv"))
                 .andReturn();
     }
+
+    @Test
+    public void testListSearchCustomAnalyticsData() throws Exception {
+        //Given
+        int pageNo = 0;
+        int pageSize = 10;
+        String sortBy = "date";
+        String order ="asc";
+
+
+        CustomAnalyticsDataFilterRequest filter = new CustomAnalyticsDataFilterRequest();
+        ObjectMapper objectMapper = new ObjectMapper();
+        String request = objectMapper.writeValueAsString(filter);
+        filter.setProductCategory("Electronics");
+
+        String path = "/custom-analytics/search";
+        CustomAnalyticsDataResponse response = new CustomAnalyticsDataResponse();
+        response.setId(1L);
+        response.setGeographicLocation("Test");
+        response.setTotalSales(1.0);
+
+        //Then
+        when(customAnalyticsService.searchBasedOnFilterAndSort(pageNo, pageSize,sortBy,order,filter)).thenReturn(List.of(response));
+
+        mockMvc.perform(get(path)
+                        .param("pageNo", String.valueOf(pageNo))
+                        .param("pageSize", String.valueOf(pageSize))
+                        .param("sortBy", sortBy)
+                        .param("order", order)
+                        .param("productCategory", filter.getProductCategory())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(request))
+
+                //When
+                .andExpect(status().isOk())
+                /*.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$[0].id", is(1)))
+                .andExpect(jsonPath("$[0].geographicLocation", is("Test")))
+                .andExpect(jsonPath("$[0].totalSales", is(1.0)))*/;
+    }
+
+    @Test
+    public void tesGetTotalSalesAndCount() throws Exception {
+        //Given
+        LocalDate fromDate = LocalDate.parse("2023-01-01");;
+        LocalDate toDate = LocalDate.parse("2023-01-02");
+        String productCategory = "Electronics";
+
+
+
+        String path = "/custom-analytics/product-category/total";
+        GetTotalProfitAndCountResponse response = new GetTotalProfitAndCountResponse();
+        response.setTotalProductCount(100.0);
+        response.setTotalProductProfit(100000.0);
+
+
+        //Then
+        when(customAnalyticsService.getTotalSalesAndCount(fromDate,toDate,productCategory)).thenReturn(response);
+
+        mockMvc.perform(get(path)
+                        .param("fromDate", String.valueOf(fromDate))
+                        .param("toDate", String.valueOf(toDate))
+
+                        .param("productCategory", String.valueOf(productCategory))
+                        .contentType(MediaType.APPLICATION_JSON))
+
+
+                //When
+                .andExpect(status().isOk())
+                //.andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("totalProductCount", is(100.0)))
+                .andExpect(jsonPath("totalProductProfit", is(100000.0)));
+    }
+
+    @Test
+    public void tesGetLocationTotalSalesAndCount() throws Exception {
+        //Given
+        LocalDate fromDate = LocalDate.parse("2023-01-01");;
+        LocalDate toDate = LocalDate.parse("2023-01-02");
+        String geographicLocation = "Test";
+
+
+
+        String path = "/custom-analytics/geographic-location/total";
+        GetTotalProfitAndCountResponse response = new GetTotalProfitAndCountResponse();
+        response.setTotalProductCount(100.0);
+        response.setTotalProductProfit(100000.0);
+
+
+        //Then
+        when(customAnalyticsService.getLocationTotalSalesAndCount(fromDate,toDate,geographicLocation)).thenReturn(response);
+
+        mockMvc.perform(get(path)
+                        .param("fromDate", String.valueOf(fromDate))
+                        .param("toDate", String.valueOf(toDate))
+
+                        .param("geographicLocation", String.valueOf(geographicLocation))
+                        .contentType(MediaType.APPLICATION_JSON))
+
+
+                //When
+                .andExpect(status().isOk())
+                //.andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("totalProductCount", is(100.0)))
+                .andExpect(jsonPath("totalProductProfit", is(100000.0)));
+    }
+
 }
